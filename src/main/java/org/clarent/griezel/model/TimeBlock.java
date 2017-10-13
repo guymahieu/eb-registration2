be4d2aa934459300a5f60628592c63e0622194c3
guy.mahieu@gmail.com <Gasma1gb!>
2017-10-13 19:34:03 +0200

    aanpassingen tijdsblokken

--- src/main/java/org/clarent/griezel/model/TimeBlock.java
+++ src/main/java/org/clarent/griezel/model/TimeBlock.java
@@ -13,7 +13,7 @@ public enum TimeBlock {
     START_AT_18_10("18:10"),
     START_AT_18_20("18:20"),
     START_AT_18_30("18:30"),
-    START_AT_18_40("18:40"),
+    START_AT_18_40("18:40", 29),
     START_AT_18_50("18:50"),
     START_AT_19_00("19:00"),
     START_AT_19_10("19:10"),
@@ -22,7 +22,7 @@ public enum TimeBlock {
     START_AT_19_40("19:40"),
     START_AT_19_50("19:50"),
     START_AT_20_00("20:00"),
-    START_AT_20_10("20:10"),
+    START_AT_20_10("20:10", 26),
     START_AT_20_20("20:20"),
     START_AT_20_30("20:30")
     ;
@@ -33,8 +33,12 @@ public enum TimeBlock {
     private final int maxPeople;
 
     TimeBlock(String display) {
+        this(display, MAX_PEOPLE);
+    }
+
+    TimeBlock(String display, int maxPeople) {
         this.display = display;
-        this.maxPeople = MAX_PEOPLE;
+        this.maxPeople = maxPeople;
     }
 
     public String getDisplay() {
